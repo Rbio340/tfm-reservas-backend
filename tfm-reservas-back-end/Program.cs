8b51f06ecfa955fa9009b66c020e3865b3f0a1b9
AlanJuker <75257308+AlanJuker@users.noreply.github.com>
2024-06-02 15:30:05 -0500

    Swagger added to the project

--- tfm-reservas-back-end/Program.cs
+++ tfm-reservas-back-end/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Reserva.Core.Interfaces.Repository.MySql;
 using Reserva.Repository.Context;
 using Reserva.Repository.Data.MySql;
@@ -18,6 +19,30 @@ builder.Services.AddScoped<IEstadoRepository, EstadoRepository>();
 builder.Services.AddScoped<IReservaRepository, ReservaRepository>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IConfiguracionRepository, ConfiguracionRepository>();
+
+// Configurar Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Reserva API",
+        Description = "An ASP.NET Core Web API for managing reservations",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,13 +50,18 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reserva API V1");
+    c.RoutePrefix = string.Empty; // para que Swagger UI esté en la raíz (opcional)
+});
 
 app.MapControllerRoute(
     name: "default",
